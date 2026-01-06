79133eb09104f520a1eaeacb8b2eb84d5c0bcc1d
Aglan <76439836+m-aglan@users.noreply.github.com>
2026-01-06 15:50:52 +0200

    Enable ignoreBuildErrors in TypeScript configignore
    Allow production builds to complete with type errors.

--- next.config.js
+++ next.config.js
@@ -10,5 +10,16 @@ const nextConfig = {
     ],
   },
 }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
+export default nextConfig;
 module.exports = nextConfig
